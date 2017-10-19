3d40eeddc1042c42ed93ab3a18dba9b2cdeb2a65
Ryan Nevius <ryanevius@gmail.com>
2017-10-19 11:55:00 -0600

    Initial commit.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Menu Item Visibility Control
+ * Plugin URI: https://github.com/rnevius/wordpress-menu-item-visibility-by-user-role
+ * Description: Hide individual menu items for certain user roles.
+ * Version: 1.0.0
+ * Author: Ryan Nevius
+ * Author URI: http://ryannevius.com
+ * Requires at least: 4.8
+ * Tested up to: 4.8
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
